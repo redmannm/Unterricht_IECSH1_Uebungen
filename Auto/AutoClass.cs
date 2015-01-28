8741a8345275a07883e46dfd2a42e95465478a15
Marcus Redmann <privat.mr@web-spirits.net>
2015-01-28 06:21:59 +0100

    X

--- Auto/AutoClass.cs
+++ Auto/AutoClass.cs
@@ -29,261 +29,94 @@ namespace AutoClass
     //Alle Eigenschaften sollen sich ausgeben lassen und auch ob der Motor läuft bzw. ob Türen geöffnet sind.
     public class Auto
     {
-        public string marke;
-        public string modell;
-        public int leistung;
+        private string marke; // Privates Feld Fahrzeug Marke
+        /// <summary>
+        /// Marke des Fahrzeugs
+        /// </summary>
+        public string Marke
+        {
+            get { return marke; }
+            set
+            {
+                // Wenn bei Abfrage ohne Eingabe Enter gedrückt wird der Wert auf
+                // aktuellen Wert belassen
+                if (value == "")
+                    marke = marke;
+                else
+                    marke = value;
+            }
+        }
+
+        private string modell; // Privates Feld Fahrzeug Modell
+        /// <summary>
+        /// Modell des Fahrzeugs
+        /// </summary>
+        public string Modell
+        {
+            get { return modell; }
+            set 
+            {
+                // Wenn bei Abfrage ohne Eingabe Enter gedrückt wird der Wert auf
+                // aktuellen Wert belassen
+                if (value == "")
+                    modell = modell;
+                else
+                    modell = value;
+            }
+        }
+        private int leistung;
+        /// <summary>
+        /// Leistung des Fahrzeugs
+        /// </summary>
+        public int Leistung
+        {
+            get { return leistung; }
+            set
+            {
+                
+            }
+        }
         public double kmStand;
         public string farbe;
         public DateTime baujahr;
-        public bool tuerOffen;
-        public bool motorAn;
+        public bool tuerStatus;
+        public bool motorStatus;
 
         public Auto()
         {
-            this.tuerOffen = false;
-            this.motorAn = false;
+            this.tuerStatus = false;
+            this.motorStatus = false;
         }
-        
+
         /// <summary>
         /// Gibt die aktuellen Fahrzeug-Eigenschaften aus.
         /// </summary>
-        public void getPkwConfig()
-        {
-            Console.WriteLine("Aktuelle Fahrzeug-Eigenschaften");
-            Console.WriteLine("-------------------------------\n");
-            
-            // Marke ausgeben
-            if (this.marke == null || this.marke == "")
-                Console.WriteLine("{0}{1}", "Marke:".PadRight(12), "Nicht gesetzt".PadLeft(15));
-            else
-                Console.WriteLine("{0}{1}", "Marke:".PadRight(12), this.marke.PadLeft(15));
-
-            // Modell ausgeben
-            if (this.modell == null || this.modell == "")
-                Console.WriteLine("{0}{1}", "Modell:".PadRight(12), "Nicht gesetzt".PadLeft(15));
-            else
-                Console.WriteLine("{0}{1}", "Modell:".PadRight(12), this.modell.PadLeft(15));
-
-            // Leistung ausgeben
-            string kw = Convert.ToString(this.leistung) + " KW";
-            string ps = Convert.ToString(this.leistung * 1.35962) + " PS";
-            Console.WriteLine("{0}{1}", "Leistung:".PadRight(12), kw.PadLeft(15));
-            Console.WriteLine("{0}{1}", "".PadRight(12), ps.PadLeft(15));
-
-            // Kilometerstand ausgeben
-            string km = Convert.ToString(this.kmStand) + " KM";
-            Console.WriteLine("{0}{1}", "KM-Stand:".PadRight(12), km.PadLeft(15));
-
-            // Farbe ausgeben
-            if (this.farbe == null)
-                Console.WriteLine("{0}{1}", "Farbe:".PadRight(12), "Nicht gesetzt".PadLeft(15));
-            else
-                Console.WriteLine("{0}{1}", "Farbe:".PadRight(12), this.farbe.PadLeft(15));
-
-            // Baujahr ausgeben
-            if (this.baujahr.ToShortDateString() == "01.01.0001")
-                Console.WriteLine("{0}{1}", "Baujahr:".PadRight(12), "Nicht gesetzt".PadLeft(15));
-            else
-                Console.WriteLine("{0}{1}", "Baujahr:".PadRight(12), Convert.ToString(this.baujahr.ToShortDateString()).PadLeft(15));
-        }
 
         /// <summary>
         /// Fahrzeug-Eigenschaften bearbeiten
         /// </summary>
-        public void setPkwConfig()
-        {
-            string tmp;
-            Console.WriteLine("Fahrzeug-Eigenschaften bearbeiten");
-            Console.WriteLine("---------------------------------\n");
-
-            //
-            // Marke eingeben
-            //---------------
-            
-            // Eingabe Meldung
-            Console.Write("Marke eingeben [" + this.marke + "]: ");
-
-            // Wenn bei Abfrage ohne Eingabe Enter gedrückt wird Leistung auf
-            // aktuellen Wert belassen
-            tmp = Console.ReadLine();
-            if (tmp == "")
-                this.marke = this.marke;
-            else
-                this.marke = tmp;
-
-            //
-            // Modell eingeben
-            //----------------
-            
-            // Eingabe Meldung
-            Console.Write("Modell eingeben [" + this.modell + "]: ");
-
-            // Wenn bei Abfrage ohne Eingabe Enter gedrückt wird Modell auf
-            // aktuellen Wert belassen
-            tmp = Console.ReadLine();
-            if (tmp == "")
-                this.modell = this.modell;
-            else
-                this.modell = tmp;
-            
-            //
-            // Leistung eingeben
-            //------------------
-            bool leistungFormatErr = false;   // Fehler Flag für Formatfehler setzen
-            bool leistungOverflowErr = false; // Fehler Flag für überlauf setzen
-            
-            // Schleife solange ausführen bis Eingaben korrekt sind
-            while (true)
-            {
-                try // Anweisungsblock versuchen abzuarbeiten
-                {
-                    // Fehler Meldungen
-                    if (leistungFormatErr)
-                        Console.WriteLine("Falsches Format eingegeben. Bitte nur Ganzzahlen eingeben.");
-                    if (leistungOverflowErr)
-                        Console.WriteLine("Respekt! Mehr als 2,147 Milliarden KW Leistung.");
-                    
-                    // Eingabe Meldung
-                    Console.Write("Leistung in KW eingeben [" + this.leistung.ToString() + "]: ");
-                    
-                    // Wenn bei Abfrage ohne Eingabe Enter gedrückt wird
-                    // Leistung auf aktuellen Wert belassen
-                    tmp = Console.ReadLine();
-                    if (tmp == "")
-                        this.leistung = this.leistung;
-                    else
-                        this.leistung = Convert.ToInt32(tmp);
-                    break; // Schleife beenden
-                }
-                catch (FormatException) // Formatierungsfehler abfangen
-                {
-                    leistungFormatErr = true;
-                }
-                catch (OverflowException) // Übergroße Werte abfangen
-                {
-                    leistungOverflowErr = true;
-                }
-            } // Schleifen ende
-
-            //
-            // Kilometerstand eingeben
-            //------------------------
-            bool kmStandFormatErr = false;   // Fehler Flag für Formatfehler setzen
-            bool kmStandOverflowErr = false; // Fehler Flag für überlauf setzen
-
-            // Schleife solange ausführen bis Eingaben korrekt sind
-            while (true)
-            {
-                try // Anweisungsblock versuchen abzuarbeiten
-                {
-                    // Fehler Meldungen
-                    if (kmStandFormatErr)
-                        Console.WriteLine("Falsches Format eingegeben. Bitte Ganzzahl oder Fließkommazahl eingeben.");
-                    if (kmStandOverflowErr)
-                        Console.WriteLine("Wert zu groß!");
-
-                    // Eingabe Meldung
-                    Console.Write("KM-Stand eingeben [" + this.kmStand.ToString() + "]: ");
-
-                    // Wenn bei Abfrage ohne Eingabe Enter gedrückt wird 
-                    // Kilometerstand auf aktuellen Wert belassen
-                    tmp = Console.ReadLine();
-                    if (tmp == "")
-                        this.kmStand = this.kmStand;
-                    else
-                        this.kmStand = Convert.ToDouble(tmp);
-                    break; // Schleife beende
-                }
-                catch (FormatException) // Formatierungsfehler abfangen
-                {
-                    kmStandFormatErr = true;
-                }
-                catch (OverflowException) // Übergroße Werte abfangen
-                {
-                    kmStandOverflowErr = true;
-                }
-            } // Schleifen ende
-            
-            //
-            // Farbe eingeben
-            //---------------
-
-            // Eingabe Meldung
-            Console.Write("Farbe eingeben [" + this.farbe + "]: ");
-
-            // Wenn bei Abfrage ohne Eingabe Enter gedrückt wird Farbe auf
-            // aktuellen Wert belassen
-            tmp = Console.ReadLine();
-            if (tmp == "")
-                this.farbe = this.farbe;
-            else
-                this.farbe = tmp;
-
-            //
-            // Baujahr eingeben
-            //-----------------
-            bool baujahrErr = false; // Fehler Flag für Formatfehler setzen
-
-            // Schleife solange ausführen bis Eingaben korrekt sind
-            while (true)
-            {
-                try // Anweisungsblock versuchen abzuarbeiten
-                {
-                    // Fehler Meldungen
-                    if (baujahrErr)
-                        Console.WriteLine("Falsches Format eingegeben. Bitte korregieren.");
-
-                    // Eingabe Meldungen
-                    Console.WriteLine("Baujahr eingeben [" + this.baujahr.ToShortDateString() + "] ");
-                    Console.WriteLine("Format: Nur Jahr (01.01.JJJJ)");
-                    Console.WriteLine("{0}{1}","".PadRight(8), "Komplettes Datum (DD.MM.JJJJ)");
-
-                    // Wenn bei Abfrage ohne Eingabe Enter gedrückt wird 
-                    // Baujahr auf aktuellen Wert belassen
-                    tmp = Console.ReadLine();
-                    if (tmp == "")
-                        this.baujahr = this.baujahr;
-                    else
-                        this.baujahr = DateTime.Parse(tmp + ".01.01");
-                    break; // Schleife beenden
-                }
-                catch (FormatException) // Formatierungsfehler abfangen
-                {
-                    try // Anweisungsblock versuchen abzuarbeiten
-                    {
-                        this.baujahr = DateTime.Parse(tmp);
-                        break;
-                    }
-                    catch (FormatException) // Formatierungsfehler abfangen
-                    {
-                        baujahrErr = true;
-                    }
-                    baujahrErr = true;
-                }
-            } // Schleifen ende
-        }
         public void TuerAction()
         {
-            if (this.tuerOffen == true)
+            if (this.tuerStatus == true)
             {
-                this.tuerOffen = false;
+                this.tuerStatus = false;
             }
             else
             {
-                this.tuerOffen = true;
+                this.tuerStatus = true;
             }
 
         }
 
         public void MotorAction()
         {
-            if (this.motorAn == true)
+            if (this.motorStatus == true)
             {
-                this.motorAn = false;
+                this.motorStatus = false;
             }
             else
             {
-                this.motorAn = true;
+                this.motorStatus = true;
             }
         }
     }
--- AutoApp/Program.cs
+++ AutoApp/Program.cs
@@ -7,14 +7,18 @@ using System.Threading.Tasks;
 
 namespace AutoApp
 {
-    class Program
+    class AutoClassDemo
     {
+        public Auto fahrzeug1 = new Auto(); // Fahrzeug erzeugen
+        
         static void Main(string[] args)
         {
+            AutoClassDemo thisApp = new AutoClassDemo();
+            
             ConsoleKeyInfo menuKey;
-            Auto fahrzeug1 = new Auto();
+            
 
-            Console.Write(getMenu(1));
+            Console.Write(getMenu(1)); // Hauptmenü aufrufen
             
             do
             {
@@ -26,7 +30,7 @@ namespace AutoApp
                 if (menuKey.Key == ConsoleKey.D1)
                 {
                     Console.Write(getMenu(1));
-                    fahrzeug1.getPkwConfig();
+                    getPkwConfig(thisApp.fahrzeug1);
                 }
 
                 //
@@ -35,7 +39,7 @@ namespace AutoApp
                 if (menuKey.Key == ConsoleKey.D2)
                 {
                     Console.Write(getMenu(1));
-                    fahrzeug1.setPkwConfig();
+                    setPkwConfig(thisApp.fahrzeug1);
                     Console.Write(getMenu(1));
                     Console.WriteLine("Die Fahrzeug-Eigenschaften wurden erfolgreich bearbeitet.");
                 }
@@ -45,14 +49,14 @@ namespace AutoApp
                 //
                 if (menuKey.Key == ConsoleKey.D3)
                 {
-                    if (fahrzeug1.tuerOffen)
+                    if (thisApp.fahrzeug1.tuerStatus)
                     {
                         Console.Write(getMenu(1));
                         Console.WriteLine("Die Fahrzeugtür ist bereits geöfnet!");
                     }
                     else
                     {
-                        fahrzeug1.TuerAction();
+                        thisApp.fahrzeug1.TuerAction();
                         Console.Write(getMenu(1));
                         Console.WriteLine("Die Fahrzeugtür wurde geöfnet!");
                     }
@@ -63,14 +67,14 @@ namespace AutoApp
                 //
                 if (menuKey.Key == ConsoleKey.D4)
                 {
-                    if (!fahrzeug1.tuerOffen)
+                    if (!thisApp.fahrzeug1.tuerStatus)
                     {
                         Console.Write(getMenu(1));
                         Console.WriteLine("Die Fahrzeugtür ist bereits geschloßen!");
                     }
                     else
                     {
-                        fahrzeug1.TuerAction();
+                        thisApp.fahrzeug1.TuerAction();
                         Console.Write(getMenu(1));
                         Console.WriteLine("Die Fahrzeugtür wurde geschloßen!");
                     }
@@ -81,22 +85,38 @@ namespace AutoApp
                 //
                 if (menuKey.Key == ConsoleKey.D5)
                 {
-                    if (fahrzeug1.motorAn)
+                    if (thisApp.fahrzeug1.motorStatus)
                     {
                         Console.Write(getMenu(1));
                         Console.WriteLine("Der Motor läuft bereits!");
                     }
                     else
                     {
-                        fahrzeug1.MotorAction();
+                        thisApp.fahrzeug1.MotorAction();
                         Console.Write(getMenu(1));
                         Console.WriteLine("Der Motor wurde gestartet!");
                     }
                 }
+
+                //
+                // Motor stoppen
+                //
+                if (menuKey.Key == ConsoleKey.D6)
+                {
+                    if (!thisApp.fahrzeug1.motorStatus)
+                    {
+                        Console.Write(getMenu(1));
+                        Console.WriteLine("Der Motor ist bereits aus!");
+                    }
+                    else
+                    {
+                        thisApp.fahrzeug1.MotorAction();
+                        Console.Write(getMenu(1));
+                        Console.WriteLine("Der Motor wurde gestopt!");
+                    }
+                }
                     
             } while (menuKey.Key != ConsoleKey.E);
-            
-            
         }
 
         /// <summary>
@@ -108,7 +128,7 @@ namespace AutoApp
         {
             string menu;
             switch (type) {
-                case 1:
+                case 1: // Hauptmenü
                     clearScreen();
                     menu =  "Anwendungsbeispiel der Klasse Auto\n" +
                             "----------------------------------\n\n" +
@@ -124,7 +144,7 @@ namespace AutoApp
                             "(6) Motor stoppen\n\n" +
                             "(E) Programm beenden\n\n";
                             return menu;
-                case 2:
+                case 2: // für ein eventuelles Submenü
                     menu = "";
                     clearScreen();
                     return menu;
@@ -136,5 +156,210 @@ namespace AutoApp
         {
             Console.Clear();
         }
+        
+        public static void getPkwConfig(Auto @fahrzeug)
+        {
+            Console.WriteLine("Aktuelle Fahrzeug-Eigenschaften");
+            Console.WriteLine("-------------------------------\n");
+            
+            // Marke ausgeben
+            if (@fahrzeug.Marke == null || @fahrzeug.Marke == "")
+                Console.WriteLine("{0}{1}", "Marke:".PadRight(12), "Nicht gesetzt".PadLeft(15));
+            else
+                Console.WriteLine("{0}{1}", "Marke:".PadRight(12), @fahrzeug.Marke.PadLeft(15));
+
+            // Modell ausgeben
+            if (@fahrzeug.Modell == null || @fahrzeug.Modell == "")
+                Console.WriteLine("{0}{1}", "Modell:".PadRight(12), "Nicht gesetzt".PadLeft(15));
+            else
+                Console.WriteLine("{0}{1}", "Modell:".PadRight(12), @fahrzeug.Modell.PadLeft(15));
+
+            // Leistung ausgeben
+            string kw = Convert.ToString(this.Leistung) + " KW";
+            string ps = Convert.ToString(this.Leistung * 1.35962) + " PS";
+            Console.WriteLine("{0}{1}", "Leistung:".PadRight(12), kw.PadLeft(15));
+            Console.WriteLine("{0}{1}", "".PadRight(12), ps.PadLeft(15));
+
+            // Kilometerstand ausgeben
+            string km = Convert.ToString(this.kmStand) + " KM";
+            Console.WriteLine("{0}{1}", "KM-Stand:".PadRight(12), km.PadLeft(15));
+
+            // Farbe ausgeben
+            if (this.farbe == null)
+                Console.WriteLine("{0}{1}", "Farbe:".PadRight(12), "Nicht gesetzt".PadLeft(15));
+            else
+                Console.WriteLine("{0}{1}", "Farbe:".PadRight(12), this.farbe.PadLeft(15));
+
+            // Baujahr ausgeben
+            if (this.baujahr.ToShortDateString() == "01.01.0001")
+                Console.WriteLine("{0}{1}", "Baujahr:".PadRight(12), "Nicht gesetzt".PadLeft(15));
+            else
+                Console.WriteLine("{0}{1}", "Baujahr:".PadRight(12), Convert.ToString(this.baujahr.ToShortDateString()).PadLeft(15));
+        }
+
+        public static void setPkwConfig(Auto @fahrzeug)
+        {
+            string tmp;
+            Console.WriteLine("Fahrzeug-Eigenschaften bearbeiten");
+            Console.WriteLine("---------------------------------\n");
+
+            //
+            // Marke eingeben
+            //---------------
+
+            Console.Write("Marke eingeben [" + Marke + "]: ");
+            Marke = Console.ReadLine();
+
+            //
+            // Modell eingeben
+            //----------------
+
+            Console.Write("\nModell eingeben [" + Modell + "]: ");
+            Modell = Console.ReadLine();
+
+            //
+            // Leistung eingeben
+            //------------------
+            bool leistungFormatErr = false;   // Fehler Flag für Formatfehler setzen
+            bool leistungOverflowErr = false; // Fehler Flag für überlauf setzen
+
+            // Schleife solange ausführen bis Eingaben korrekt sind
+            while (true)
+            {
+                try // Anweisungsblock versuchen abzuarbeiten
+                {
+                    // Fehler Meldungen
+                    if (leistungFormatErr)
+                        Console.WriteLine("Falsches Format eingegeben. Bitte nur Ganzzahlen eingeben.");
+                    if (leistungOverflowErr)
+                        Console.WriteLine("Respekt! Mehr als 2,147 Milliarden KW Leistung.");
+
+                    // Eingabe Meldung
+                    Console.Write("\nLeistung in KW eingeben [" + this.Leistung.ToString() + "]: ");
+
+                    // Wenn bei Abfrage ohne Eingabe Enter gedrückt wird
+                    // Leistung auf aktuellen Wert belassen
+                    tmp = Console.ReadLine();
+                    if (tmp == "")
+                        this.Leistung = this.Leistung;
+                    else
+                        this.Leistung = Convert.ToInt32(tmp);
+                    break; // Schleife beenden
+                }
+                catch (FormatException) // Formatierungsfehler abfangen
+                {
+                    leistungFormatErr = true;
+                }
+                catch (OverflowException) // Übergroße Werte abfangen
+                {
+                    leistungOverflowErr = true;
+                }
+            } // Schleifen ende
+
+            //
+            // Kilometerstand eingeben
+            //------------------------
+            bool kmStandFormatErr = false;   // Fehler Flag für Formatfehler setzen
+            bool kmStandOverflowErr = false; // Fehler Flag für überlauf setzen
+
+            // Schleife solange ausführen bis Eingaben korrekt sind
+            while (true)
+            {
+                try // Anweisungsblock versuchen abzuarbeiten
+                {
+                    // Fehler Meldungen
+                    if (kmStandFormatErr)
+                        Console.WriteLine("Falsches Format eingegeben. Bitte Ganzzahl oder Fließkommazahl eingeben.");
+                    if (kmStandOverflowErr)
+                        Console.WriteLine("Wert zu groß!");
+
+                    // Eingabe Meldung
+                    Console.Write("\nKM-Stand eingeben [" + this.kmStand.ToString() + "]: ");
+
+                    // Wenn bei Abfrage ohne Eingabe Enter gedrückt wird 
+                    // Kilometerstand auf aktuellen Wert belassen
+                    tmp = Console.ReadLine();
+                    if (tmp == "")
+                        this.kmStand = this.kmStand;
+                    else
+                        this.kmStand = Convert.ToDouble(tmp);
+                    break; // Schleife beende
+                }
+                catch (FormatException) // Formatierungsfehler abfangen
+                {
+                    kmStandFormatErr = true;
+                }
+                catch (OverflowException) // Übergroße Werte abfangen
+                {
+                    kmStandOverflowErr = true;
+                }
+            } // Schleifen ende
+
+            //
+            // Farbe eingeben
+            //---------------
+
+            // Eingabe Meldung
+            Console.Write("\nFarbe eingeben [" + this.farbe + "]: ");
+
+            // Wenn bei Abfrage ohne Eingabe Enter gedrückt wird Farbe auf
+            // aktuellen Wert belassen
+            tmp = Console.ReadLine();
+            if (tmp == "")
+                this.farbe = this.farbe;
+            else
+                this.farbe = tmp;
+
+            //
+            // Baujahr eingeben
+            //-----------------
+            bool baujahrErr = false; // Fehler Flag für Formatfehler setzen
+
+            // Schleife solange ausführen bis Eingaben korrekt sind
+            while (true)
+            {
+                try // Anweisungsblock versuchen abzuarbeiten
+                {
+                    // Fehler Meldungen
+                    if (baujahrErr)
+                        Console.WriteLine("Falsches Format eingegeben. Bitte korregieren.");
+
+                    // Eingabe Meldungen
+                    if (this.baujahr.ToShortDateString() == "01.01.0001")
+                        tmp = "Nicht gesezt";
+                    else
+                        tmp = this.baujahr.ToShortDateString();
+                    Console.WriteLine("\nBaujahr eingeben [" + tmp + "] ");
+                    Console.WriteLine("Format: Nur Jahr (bsp. 1999)");
+                    Console.WriteLine("{0}{1}", "".PadRight(8), "Komplettes Datum (bsp. 11.10.1974)");
+
+                    // Wenn bei Abfrage ohne Eingabe Enter gedrückt wird 
+                    // Baujahr auf aktuellen Wert belassen
+                    tmp = Console.ReadLine();
+                    if (tmp == "")
+                        this.baujahr = this.baujahr;
+                    else
+                        this.baujahr = DateTime.Parse(tmp + ".01.01");
+                    break; // Schleife beenden
+                }
+                catch (FormatException) // Formatierungsfehler abfangen
+                {
+                    try // Anweisungsblock versuchen abzuarbeiten
+                    {
+                        this.baujahr = DateTime.Parse(tmp);
+                        break;
+                    }
+                    catch (FormatException) // Formatierungsfehler abfangen
+                    {
+                        baujahrErr = true;
+                    }
+                    baujahrErr = true;
+                }
+            } // Schleifen ende
+        }
+
+        
+
+
     }
 }

1d45a58075f3e162a2e5d2c9f392a7943b719a4d
Marcus Redmann <privat.mr@web-spirits.net>
2015-03-04 13:30:28 +0100

    X

---
+++ BS_IEWCS1_Marcus_Redmann/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BS_IEWCS1_Marcus_Redmann
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Mitarbeiter> _mitarbeiterCollection = new List<Mitarbeiter>();
+
+        internal List<Mitarbeiter> MitarbeiterCollection
+        {
+            get
+            {
+                return _mitarbeiterCollection;
+            }
+            set
+            {
+                _mitarbeiterCollection = value;
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            
+
+        }
+
+        public void btnSpeichern_Click(object sender, RoutedEventArgs e)
+        {
+            lstMitarbeiter.ItemsSource = GetMitarbeiter();
+            
+           
+        }
+
+        private List<Mitarbeiter> GetMitarbeiter()
+        {
+            Mitarbeiter NeuerMitarbeiter = new Mitarbeiter();
+
+            NeuerMitarbeiter.Name = txtName.Text;
+            NeuerMitarbeiter.Vorname = txtVorname.Text;
+            NeuerMitarbeiter.Strasse = txtStrasse.Text;
+            NeuerMitarbeiter.Hausnummer = txtHausnummer.Text;
+            NeuerMitarbeiter.Plz = txtPlz.Text;
+            NeuerMitarbeiter.Ort = txtOrt.Text;
+            NeuerMitarbeiter.Geburtsdatum = txtGeburtsdatum.Text;
+            NeuerMitarbeiter.Tel = txtTelefon.Text;
+            NeuerMitarbeiter.Konfession = txtKonfession.Text;
+            NeuerMitarbeiter.LohnsteuerKlasse = (LohnStKlasse)Convert.ToInt32(txtLohnsteuerKlasse.Text);
+            NeuerMitarbeiter.Kinder = Convert.ToInt32(txtKinder.Text);
+            NeuerMitarbeiter.BruttoGehalt = Convert.ToDouble(txtBruttogehalt.Text);
+            NeuerMitarbeiter.NettoGehalt = GetNetto(NeuerMitarbeiter);
+
+            MitarbeiterCollection.Add(NeuerMitarbeiter);
+
+            return MitarbeiterCollection;
+        }
+
+        private double GetNetto(Mitarbeiter mitarbeiter)
+        {
+            double netto = 0;
+
+            if (mitarbeiter.LohnsteuerKlasse == LohnStKlasse.Eins)
+            {
+                netto = mitarbeiter.BruttoGehalt * 0.79;
+            }
+            else if (mitarbeiter.LohnsteuerKlasse == LohnStKlasse.Zwei)
+            {
+                netto = mitarbeiter.BruttoGehalt * 0.80;
+            }
+            else if (mitarbeiter.LohnsteuerKlasse == LohnStKlasse.Drei)
+            {
+                netto = mitarbeiter.BruttoGehalt * 0.81;
+            }
+            else if (mitarbeiter.LohnsteuerKlasse == LohnStKlasse.Vier)
+            {
+                netto = mitarbeiter.BruttoGehalt * 0.77;
+            }
+            else if (mitarbeiter.LohnsteuerKlasse == LohnStKlasse.Fuenf)
+            {
+                netto = mitarbeiter.BruttoGehalt * 0.79;
+            }
+            else if (mitarbeiter.LohnsteuerKlasse == LohnStKlasse.Sechs)
+            {
+                netto = mitarbeiter.BruttoGehalt * 0.70;
+            }
+            return netto;
+        }
+
+    }
+}
